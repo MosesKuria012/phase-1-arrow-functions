e789744254605247fad82a7c51ce4bfa9441776a
Moses <t.content012@gmail.com>
2023-03-19 15:10:57 +0300

    arrow function

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function (num1, num2){
+    return num1/num2
+}
+console.log(divide(2000,100))
+
+const square = (num3) =>{
+    return num3*num3
+}
+console.log(square(4))
+
+const add = (num4, num5) =>{
+    return num4+num5
+}
+console.log(add(5,5))
